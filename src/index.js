6f2524c1a71d95d08516895df160dca02b5fb6f5
Schnitzel <rw21062006@gmail.com>
2023-09-03 16:04:33 +0200

    read author and desc

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
 var fs = require('fs');
 var zlib = require('zlib');
 
+// 1 for short array, 2 for medium, 3 for long
+function readStringArray(type, buffer, startingPos) {
+  // startingPos needs to be passed by reference, hence array.
+  if (!Array.isArray(startingPos)) {
+    throw new Error("startingPos must be passed as an array.");
+  }
+
+  let strLen;
+  switch (type) {
+    case 1: 
+      strLen = buffer.readUInt8(startingPos[0]);
+      startingPos[0] += 1;
+      break;
+    case 2:
+      strLen = buffer.readUInt16BE(startingPos[0]);
+      startingPos[0] += 2;
+      break;
+    case 3:
+      strLen = buffer.readUInt32BE(startingPos[0]);
+      startingPos[0] += 4;
+      break;
+  }
+
+  str = "";
+  for (let i = 0; i < strLen; i++) {
+    str += String.fromCharCode(buffer.readUInt8(startingPos[0]));
+    startingPos[0]++;
+  }
+  return str;
+}
+
 function parser(fileName) {
   fs.open(fileName, 'r', function(status, fd) {
     if (status) {
@@ -22,40 +53,32 @@ function parser(fileName) {
       }
 
       if (!(signature[0] === expectedSig[0] && signature[1] === expectedSig[1] && signature[2] === expectedSig[2])) {
-        console.log("Signature does not match 66 83 80. Instead, it is \"" + signature.join(" ") + "\"");
-        return;
-      } else {
-        console.log("Signature matches 66 83 80!");
+        throw new Error("Signature does not match 66 83 80. Instead, it is \"" + signature.join(" ") + "\"");
       }
+      console.log("Signature matches 66 83 80!");
 
       const fileVersion = fileBuffer.readUInt32BE(position);
       position += 4;
       console.log("File version is " + fileVersion);
 
-      const blobSize = fileBuffer.readUint32BE(position);
+      const blobSize = fileBuffer.readUInt32BE(position);
       position += 4;
       console.log("Uncompressed blob size should be " + blobSize + " bytes.");
 
       // decompress
       const uncompressedBlob = zlib.inflateSync(fileBuffer.buffer.slice(position));
       if (!uncompressedBlob.byteLength === blobSize) {
-        console.log("Uncompressed blob size does not match expected size (" + uncompressedBlob.byteLength + ", " + blobSize + ")");
-        return;
-      } else {
-        console.log("Uncompressed blob size matches expected size!")
+        throw new Error("Uncompressed blob size does not match expected size (" + uncompressedBlob.byteLength + ", " + blobSize + ")");
       }
+      console.log("Uncompressed blob size matches expected size!")
 
-      uncompressedPos = 0;  //new position tracker for the uncompressed blob
-      buildNameStrLen = uncompressedBlob.readUint8(uncompressedPos);
-      uncompressedPos += 1;
-
-      // read build name chars
-      buildName = "";
-      for (let i = 0; i < buildNameStrLen; i++) {
-        buildName += String.fromCharCode(uncompressedBlob.readUInt8(uncompressedPos));
-        uncompressedPos++;
-      }
+      uncompressedPos = [0];  //new position tracker for the uncompressed blob, as an array so it can be passed by reference
+      buildName = readStringArray(1, uncompressedBlob, uncompressedPos);
       console.log("Build name is \"" + buildName + "\"");
+      authorName = readStringArray(1, uncompressedBlob, uncompressedPos);
+      console.log("Author name is \"" + authorName + "\"");
+      description = readStringArray(2, uncompressedBlob, uncompressedPos);
+      console.log("Description is \"" + description + "\"");
     });
   });
 }
