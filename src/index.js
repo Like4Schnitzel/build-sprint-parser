c8fe50ed1c315ae99af72f1621537f4a0ab09b63
Schnitzel <rw21062006@gmail.com>
2023-09-03 08:39:42 +0200

    check signature

--- src/index.js
+++ src/index.js
@@ -7,6 +7,21 @@ fs.open('data/11.dat', 'r', function(status, fd) {
   }
   var buffer = Buffer.alloc(100);
   fs.read(fd, buffer, 0, 100, 0, function(err, num) {
-    console.log(buffer.toString('utf-8', 0, num));
+    let position;
+
+    // check that file signature is correct
+    // first 3 bytes must be "66, 83, 80"
+    const expectedSig = [66, 83, 80];
+    let signature = [];
+    for (position = 0; position < 3; position++) {
+      signature.push(buffer.readInt8(position));
+    }
+
+    if (!(signature[0] === expectedSig[0] && signature[1] === expectedSig[1] && signature[2] === expectedSig[2])) {
+      console.log("Signature does not match 66 83 80. Instead, it is \"" + signature.join(" ") + "\"");
+      return;
+    } else {
+      console.log("Signature matches 66 83 80!");
+    }
   });
 });
